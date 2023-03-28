cda429bd84e705e91f3bdf39ab68e273f5ebb27b
sslluuccaa <soelucas3@gmail.com>
2023-03-28 01:18:22 -0500

    Veinteysiete commit

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ articles.addEventListener("mouseout", () => {
   });
 });
 
-// 
+//
 var enlaces = document.getElementsByClassName("enlace");
 var contenido = document.getElementById("contenido");
 
@@ -35,28 +35,28 @@ var contenido = document.getElementById('contenido');
 
 // agrega un evento de clic al botón que muestra el contenido adicional
 boton.addEventListener('click', function() {
-  // inserta el contenido adicional después del contenedor existente
-  contenido.insertAdjacentHTML('afterend', boton.dataset.contenido);
-  
-  // oculta el botón "Mostrar más"
-  boton.style.display = 'none';
+// inserta el contenido adicional después del contenedor existente
+contenido.insertAdjacentHTML('afterend', boton.dataset.contenido);
+
+// oculta el botón "Mostrar más"
+boton.style.display = 'none';
 });
 
-// 
+//
 var enlaces = document.getElementsByClassName("enlace");
 
 for (var i = 0; i < enlaces.length; i++) {
-  enlaces[i].addEventListener("click", function(event) {
-    event.preventDefault(); // Evita que el enlace siga el enlace
+enlaces[i].addEventListener("click", function(event) {
+event.preventDefault(); // Evita que el enlace siga el enlace
 
-    // Crea un nuevo div para el contenido adicional
-    var nuevoContenido = document.createElement("div");
-    nuevoContenido.classList.add("contenido");
-    nuevoContenido.innerHTML = this.getAttribute("data-contenido");
+// Crea un nuevo div para el contenido adicional
+var nuevoContenido = document.createElement("div");
+nuevoContenido.classList.add("contenido");
+nuevoContenido.innerHTML = this.getAttribute("data-contenido");
 
-    // Agrega el nuevo contenido después del enlace actual
-    this.insertAdjacentElement("afterend", nuevoContenido);
-  });
+// Agrega el nuevo contenido después del enlace actual
+this.insertAdjacentElement("afterend", nuevoContenido);
+});
 }
 
 // const articleContainer = document.querySelector('.article-container');
@@ -64,42 +64,36 @@ const articleExpandedContainer = document.querySelector('.article-expanded-conta
 const toggleButton = document.querySelector('#toggle-button');
 
 toggleButton.addEventListener('click', () => {
-  if (articleContainer.style.display !== 'none') {
-    articleContainer.style.display = 'none';
-    articleExpandedContainer.style.display = 'block';
-  } else {
-    articleContainer.style.display = 'block';
-    articleExpandedContainer.style.display = 'none';
-  }
+if (articleContainer.style.display !== 'none') {
+articleContainer.style.display = 'none';
+articleExpandedContainer.style.display = 'block';
+} else {
+articleContainer.style.display = 'block';
+articleExpandedContainer.style.display = 'none';
+}
 });
 
 //
 const toggleButtons = document.querySelectorAll('[id^="toggle-button-"]');
 toggleButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    const articleCard = button.parentNode;
-    articleCard.classList.toggle('expanded');
-  });
+button.addEventListener('click', () => {
+const articleCard = button.parentNode;
+articleCard.classList.toggle('expanded');
+});
 });
 
- // Obtener todos los botones "Saber más"
-  var botones = document.querySelectorAll("#table-board .enlace");
+// Obtener todos los botones "Saber más"
+var botones = document.querySelectorAll("#table-board .enlace");
 
-  // Recorrer todos los botones y agregar el evento click
-  botones.forEach(function(boton) {
-    boton.addEventListener("click", function(event) {
-      event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-      var contenido = boton.dataset.contenido; // Obtener el contenido del atributo data-contenido
-      var contenedor = document.querySelector("#contenido"); // Obtener el contenedor donde se mostrará el contenido
-      contenedor.innerHTML = contenido; // Agregar el contenido al contenedor
-    });
-  });
-
-// Cerrar botón de la sección de categorías
-  const cerrarBtn = document.getElementById('toggle-button');
-  cerrarBtn.addEventListener('click', () => {
-    // Agrega aquí la función para traer la lista de aplicaciones de IA para la categoría correspondiente
-  });
+// Recorrer todos los botones y agregar el evento click
+botones.forEach(function(boton) {
+boton.addEventListener("click", function(event) {
+	event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+	var contenido = boton.dataset.contenido; // Obtener el contenido del atributo data-contenido
+	var contenedor = document.querySelector("#contenido"); // Obtener el contenedor donde se mostrará el contenido
+	contenedor.innerHTML = contenido; // Agregar el contenido al contenedor
+});
+});
 
   // 
   $('.imagen-principal').click(function() {
@@ -129,10 +123,25 @@ subContenido.addEventListener("click", () => {
 subContenido.classList.remove("mostrar");
 });
 
-
+// 
 $(document).ready(function(){
-    $("#subtabla td").click(function(){
-        var contenido = $(this).find(".contenido").html();
-        $("#subcontenido").html(contenido);
-    });
+$("#subtabla td").click(function(){
+	var contenido = $(this).find(".contenido").html();
+	$("#subcontenido").html(contenido);
+});
+});
+
+// 
+var enlaces = document.querySelectorAll('.subenlace');
+enlaces.forEach(function(enlace) {
+enlace.addEventListener('click', function(e) {
+	e.preventDefault();
+	var contenido = this.getAttribute('data-contenido');
+	var subtabla = document.getElementById(subcontenido);
+	var subtablas = document.querySelectorAll('.subtable-board');
+	subtablas.forEach(function(tabla) {
+	tabla.classList.remove('activo');
+	});
+	subtabla.classList.add('activo');
+});
 });
