2ca353b1112a2643d23d88192004504b686be473
lucfreelance <lucfreelance@gmail.com>
2023-04-04 22:55:16 -0500

    treinta y siete

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ articles.addEventListener("mouseout", () => {
 
 //
 var enlaces = document.getElementsByClassName("enlace");
-var contenido = document.getElementById("contenido");
+var contenido = document.getElementById("contenidos");
 
 for (var i = 0; i < enlaces.length; i++) {
   enlaces[i].addEventListener("click", function(event) {
@@ -28,42 +28,6 @@ for (var i = 0; i < enlaces.length; i++) {
     contenido.innerHTML = this.getAttribute("data-contenido"); // Actualiza el contenido
   });
 }
-
-// selecciona el botón "Mostrar más" y el contenedor adicional
-var boton = document.getElementById('toggle-button');
-var contenido = document.getElementById('contenido');
-
-// agrega un evento de clic al botón que muestra el contenido adicional
-boton.addEventListener('click', function() {
-contenido.insertAdjacentHTML('afterend', boton.dataset.contenido);
-// inserta el contenido adicional después del contenedor existente
-
-// oculta el botón "Mostrar más"
-boton.style.display = 'none';
-});
-
-// const articleContainer = document.querySelector('.article-container');
-const articleExpandedContainer = document.querySelector('.article-expanded-container');
-const toggleButton = document.querySelector('#toggle-button');
-
-toggleButton.addEventListener('click', () => {
-if (articleContainer.style.display !== 'none') {
-articleContainer.style.display = 'none';
-articleExpandedContainer.style.display = 'block';
-} else {
-articleContainer.style.display = 'block';
-articleExpandedContainer.style.display = 'none';
-}
-});
-
-//
-const toggleButtons = document.querySelectorAll('[id^="toggle-button-"]');
-toggleButtons.forEach((button, index) => {
-button.addEventListener('click', () => {
-const articleCard = button.parentNode;
-articleCard.classList.toggle('expanded');
-});
-});
 
 // Recorrer todos los botones y agregar el evento click
 botones.forEach(function(boton) {
@@ -73,34 +37,6 @@ boton.addEventListener("click", function(event) {
 	var contenedor = document.querySelector("#contenido"); // Obtener el contenedor donde se mostrará el contenido
 	contenedor.innerHTML = contenido; // Agregar el contenido al contenedor
 });
-});
-
-// 
-  $('.imagen-principal').click(function() {
-  $('#modal-imagen').modal('show');
-});
-
-// 
-// Selecciona los elementos con la clase .subenlace dentro de la sección
-const subenlaces = document.querySelectorAll('section .subenlace');
-
-// Añade un evento click a cada enlace
-subenlaces.forEach(subenlace => {
-subenlace.addEventListener('click', e => {
-e.preventDefault();
-const contenido = subenlace.getAttribute('data-contenido');
-const subContenido = document.querySelector("#subcontenido");
-subContenido.innerHTML = contenido;
-subContenido.classList.add("mostrar");
-});
-});
-
-// Selecciona el elemento con el id #subcontenido
-const subContenido = document.querySelector("#subcontenido");
-
-// Añade un evento click al #subcontenido para cerrarlo
-subContenido.addEventListener("click", () => {
-subContenido.classList.remove("mostrar");
 });
 
 // 
@@ -127,24 +63,3 @@ document.getElementById("scrollToTopButton").addEventListener("click", function(
     behavior: "smooth"
   });
 });
-
-//
-$(document).ready(function() {
-  $('.enlace').on('click', function(e) {
-    e.preventDefault();
-    var contenido = $(this).data('contenido');
-    $('#modal .modal-body').html(contenido);
-    $('#modal').modal('show');
-  });
-});
-
-// //
-// $(document).ready(function() {
-//   $('.enlace').click(function() {
-//     $(this).siblings('.contenido-enlaces').fadeIn('fast');
-//   });
-  
-//   $('.cerrar-app-btn button').click(function() {
-//     $(this).closest('.contenido-enlaces').fadeOut('fast');
-//   });
-// });
