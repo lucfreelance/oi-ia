53de401bbce9e624416f1209aaa5431047a881d3
sslluuccaa <soelucas3@gmail.com>
2023-05-17 10:42:52 -0500

    171

--- table-board/src/posts/about.md
+++
@@ -1,6 +0,0 @@
-# Hello
-
-Bienvenido a table-board. Un placer tenerte.<br>
-Encuentra la app / web de IA que más te guste.
-
-Visita mi [Repositorio Github](https://github.com/lucfreelance).
--- table-board/src/types/Post.ts
+++
@@ -1,9 +0,0 @@
-export default interface Post
-  extends Pick<
-    Queries.MarkdownRemarkFrontmatter,
-    "title" | "desc" | "date" | "category" | "alt"
-  > {
-  id: string
-  slug: Queries.MarkdownRemarkFields["slug"]
-  thumbnail?: string
-}
